11a308adafcae3463b4c9b839874fc3e86ba866f
Brett McKay <brett.mckay@avanade.com>
2017-02-11 18:18:43 -0700

    Add interfaces slides

---
+++ 11-interfaces/interface.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// EMPTY_BEGIN OMIT
+type empty interface{}
+
+// EMPTY_END OMIT
+
+// DECLARE_BEGIN OMIT
+type Stringer interface {
+	ToString() string
+}
+
+// DECLARE_END OMIT
+
+// IMPLEMENT_BEGIN OMIT
+type Simple string
+
+func (s Simple) ToString() string {
+	return string(s)
+}
+
+type Complex struct {
+	Field1 int
+	Field2 string
+	Field3 float32
+}
+
+func (c Complex) ToString() string {
+	return fmt.Sprintf("f1: %v, f2: %v, f3: %v now: %v", c.Field1, c.Field2, c.Field3, time.Now().Format("15:04:05"))
+}
+
+// IMPLEMENT_END OMIT
+
+// USE_BEGIN OMIT
+func main() {
+	c := Complex{
+		1,
+		"complex",
+		2.2,
+	}
+	strs := []Stringer{Simple("simple"), c}
+
+	printStrings(strs)
+}
+
+func printStrings(strs []Stringer) {
+	for _, s := range strs {
+		fmt.Println(s.ToString())
+	}
+}
+
+// USE_END OMIT

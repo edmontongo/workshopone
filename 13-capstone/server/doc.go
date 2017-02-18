cf7081ca650454ef770dc03e0989a7c3098dd92a
Matthias Stone <matthias@bellstone.ca>
2017-02-18 06:57:18 -0700

    Update capstone server documentation with final design

--- 13-capstone/server/doc.go
+++ 13-capstone/server/doc.go
@@ -1,4 +1,17 @@
-// Command server provides...
+// Command server provides a basic RESTesque API for participants to practice
+// writing simple Go programs against.
+//
+// Basic HTTP API sketch
+//   GET  /task/           - Provides documentation
+//   GET  /task/?name=bob  - Gets a new task ID
+//   POST /task/id         - Post form data in reply
+//   GET  /task/square/    - Gets documentation
+//   GET  /task/square/id  - Gets a number to square
+//   POST /task/square/id  - Post your squared number
+//
+//   GET  /status         - HTML showing status
+//   GET  /status/updates - Websocket providing updates to status
+//
 package main
 
 import "io"

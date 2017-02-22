1279034823c6e112a63d4de6af81a11e5035c2f1
Lukasz Rozycki <git@lukaszr.com>
2017-02-22 11:21:41 -0700

    Fixed wrong types in the solution, removed unfinished tests

--- 07-map/ex2.go
+++ 07-map/ex2.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
 
@@ -21,7 +20,7 @@ func validateDeposit(bank map[string]int32, name string, amount int32) bool {
 // DEPOSIT_E OMIT
 
 // WITHDRAW_S OMIT
-func withdraw(bank map[string]int, name string, amount int, maxOverdraft int) bool {
+func withdraw(bank map[string]int32, name string, amount int32, maxOverdraft int32) bool {
 	if !validatewithdrawal(bank, name, amount, maxOverdraft) {
 		return false
 	}
@@ -29,7 +28,7 @@ func withdraw(bank map[string]int, name string, amount int, maxOverdraft int) bo
 	return true
 }
 
-func validatewithdrawal(bank map[string]int, name string, amount int, maxOverdraft int) bool {
+func validatewithdrawal(bank map[string]int32, name string, amount int32, maxOverdraft int32) bool {
 	_, ok := bank[name]
 	return ok && amount >= 0 && math.MinInt32+amount < bank[name] &&
 		bank[name]-amount >= maxOverdraft
@@ -38,48 +37,5 @@ func validatewithdrawal(bank map[string]int, name string, amount int, maxOverdra
 // WITHDRAW_E OMIT
 
 func main() {
-	TestDeposit()
-}
 
-func TestDeposit() {
-	pass := true
-	for _, test := range depositTestData {
-		bank := map[string]int32{}
-		for k, v := range test.inbank {
-			bank[k] = v
-		}
-		ret := deposit(bank, test.name, test.amount)
-		if ret == test.ret && len(bank) == len(test.outbank) {
-			for k, v := range test.outbank {
-				if bank[k] != v {
-					pass = false
-				}
-			}
-		} else {
-			pass = false
-		}
-		if !pass {
-			fmt.Println("Fail withdraw(", test.inbank, ", \"", test.name, "\", ", test.amount, ")")
-			fmt.Println(" expected ", test.ret, ", ", test.outbank)
-			fmt.Println(" got ", ret, ",", bank)
-			break
-		}
-		fmt.Println("Pass withdraw(", test.inbank, ", \"", test.name, "\", ", test.amount, ")")
-		fmt.Println(" == ", test.ret, ", ", test.outbank)
-	}
-}
-
-var depositTestData = []struct {
-	inbank  map[string]int32
-	name    string
-	amount  int32
-	ret     bool
-	outbank map[string]int32
-}{
-	{
-		map[string]int32{"Test": 100}, "Test", 100, true, map[string]int32{"Test": 200},
-	},
-	{
-		map[string]int32{"Test": 100}, "Test", 100, true, map[string]int32{"Test": 201},
-	},
 }

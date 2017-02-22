e3a770ec1c054d23fce7c9be2b2cdde4da379e84
Matthias Stone <matthias@bellstone.ca>
2017-02-22 06:39:05 -0700

    Add multiply problem type to introduce JSON decoding

--- 13-capstone/server/doc.go
+++ 13-capstone/server/doc.go
@@ -2,14 +2,16 @@
 // writing simple Go programs against.
 //
 // Basic HTTP API sketch
-//   GET  /task/                - Provides documentation
-//   GET  /task/?name=[name]    - Gets a new task token
-//   POST /task/[token]         - Post form data in reply
-//   GET  /task/square/         - Gets documentation
-//   GET  /task/square/[token]  - Gets a number to square
-//   POST /task/square/[token]  - Post your squared number
-//   GET  /task/frequency/[token]  - Gets a paragraph to analyze
-//   POST /task/frequency/[token]  - Post your top three words
+//   GET  /task/                  - Provides documentation
+//   GET  /task/?name=[name]      - Gets a new task token
+//   POST /task/[token]           - Post form data in reply
+//   GET  /task/square/           - Gets documentation
+//   GET  /task/square/[token]    - Gets a number to square
+//   POST /task/square/[token]    - Post your squared number
+//   GET  /task/frequency/[token] - Gets a paragraph to analyze
+//   POST /task/frequency/[token] - Post your top three words
+//   GET  /task/multiply/[token]  - Gets integers to multiply
+//   POST /task/multiply/[token]  - Post your integers multiplied
 //
 //   GET  /status         - HTML showing status
 //   GET  /status/updates - Websocket providing updates to status
@@ -31,6 +33,7 @@ The next problems are at the following locations, and must be done in
 the order provided:
   /task/square/
   /task/frequency/
-Using GET on the raw URLs will provide specific instructions.
+  /task/multiply/
+Using GET on the above URLs will provide task specific instructions.
 `)
 }

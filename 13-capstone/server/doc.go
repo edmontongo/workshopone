5e481360355e029a0789693b0beb35f8f5b8b418
Matthias Stone <matthias@bellstone.ca>
2017-02-21 20:32:47 -0700

    Implement wordcount frequency problem

--- 13-capstone/server/doc.go
+++ 13-capstone/server/doc.go
@@ -8,6 +8,8 @@
 //   GET  /task/square/         - Gets documentation
 //   GET  /task/square/[token]  - Gets a number to square
 //   POST /task/square/[token]  - Post your squared number
+//   GET  /task/frequency/[token]  - Gets a paragraph to analyze
+//   POST /task/frequency/[token]  - Post your top three words
 //
 //   GET  /status         - HTML showing status
 //   GET  /status/updates - Websocket providing updates to status

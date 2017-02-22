af298770ccf24b05f5fd8f8b3ce26c921454a4a2
Nathan Youngman <git@nathany.com>
2017-02-22 08:51:37 -0700

    solutions to basics

---
+++ 02-basics/bots.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Siri: What can I help you with?")
+	fmt.Println("Google: World domination")
+	fmt.Println("Siri: Here's some information:")
+	fmt.Println("Google: Join me.")
+	fmt.Println("Siri: I didn't quite get that.")
+}
